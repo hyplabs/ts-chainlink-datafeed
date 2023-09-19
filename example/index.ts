adae0211cb6b3d98ae068ae9169fd3df752cf79f
thevolcanomanishere <alex.mcgonagle.dev@gmail.com>
2023-09-19 18:25:57 +0100

    Update example text

--- example/index.ts
+++ example/index.ts
@@ -3,7 +3,7 @@ import { polygon } from "viem/chains";
 
 const chainLinkDataFeed = new ChainLinkDataFeed({
   chain: polygon,
-  contractAddress: "0xf9680d99d6c9589e2a93a78a04a279e509205945",
+  contractAddress: "0xf9680d99d6c9589e2a93a78a04a279e509205945", // ETH / USD on polygon
   rpcUrls: [
     "https://polygon.llamarpc.com",
     "https://polygon-bor.publicnode.com",
@@ -11,7 +11,9 @@ const chainLinkDataFeed = new ChainLinkDataFeed({
   ],
 });
 
+// Check the chain every 3 seconds for new data
 const roundDataGenerator = chainLinkDataFeed.getRoundDataInterval(3);
+// Each time there is new data, log it to the console
 for await (const roundData of roundDataGenerator) {
   console.log(roundData);
 }
