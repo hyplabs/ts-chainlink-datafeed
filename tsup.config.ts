a8d9372adcd89e22ddda927c5416ec0e71921a37
thevolcanomanishere <alex.mcgonagle.dev@gmail.com>
2024-01-29 17:02:18 +0000

    Dont generate sourcemaps

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   splitting: true,
-  sourcemap: true,
+  sourcemap: false,
   treeshake: true,
   clean: true,
 });
